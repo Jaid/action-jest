79785332eea484918e162464d0fba6d705226702
Jaid <jaid.jsx@gmail.com>
2019-11-18 12:33:28 +0100

    Rebuilt src/
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- build/index.js
+++ build/index.js
@@ -1,1 +1,1 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault"),_execa=_interopRequireDefault(require("execa"));async function main(){await(0,_execa.default)("jest --help")}main();
+"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault"),_execa=_interopRequireDefault(require("execa"));async function main(){await(0,_execa.default)("npx jest --help")}main();
