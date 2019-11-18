28c73d25e39bea4a0df530674822366d208f519c
Jaid <jaid.jsx@gmail.com>
2019-11-18 05:27:52 +0100

    Transpiled src to build
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- build/index.js
+++ build/index.js
@@ -2,4 +2,24 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
 var _jest = _interopRequireDefault(require("jest"));
+
+function main() {
+  return _regenerator["default"].async(function main$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return _regenerator["default"].awrap(_jest["default"].runCLI());
+
+        case 2:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+}
+
+main();
