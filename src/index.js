00dba291ce10d9a486795c5bbf12055e849bb2e7
Jaid <jaid.jsx@gmail.com>
2019-11-19 09:02:40 +0100

    Testing
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ async function main() {
   const jestDependencyFile = path.resolve("node_modules", "jest", "bin", "jest.js")
   const isJestInstalled = await fsp.pathExists(jestDependencyFile)
   if (isJestInstalled) {
-    await exec("node", [jestDependencyFile, "--", ...jestArgs])
+    await exec("node", [jestDependencyFile, ...jestArgs])
   } else {
     const npxPath = await which("npx", true)
     console.warn("Jest not found in %s, using %s instead to install and run it", jestDependencyFile, npxPath)
