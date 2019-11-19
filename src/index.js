1526518639c67a92de07dd72502dd421cdc15dc3
Jaid <jaid.jsx@gmail.com>
2019-11-19 08:58:09 +0100

    Added Jest arguments
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import {exec} from "@actions/exec"
 import {which} from "@actions/io"
 
 async function main() {
+  const jestArgs = [
+    "--passWithNoTests",
+    "--coverage",
+    "--outputFile",
+    path.resolve("dist", "jest", "stats.json"),
+  ]
   const jestDependencyFile = path.resolve("node_modules", "jest", "bin", "jest.js")
   const isJestInstalled = await fsp.pathExists(jestDependencyFile)
   if (isJestInstalled) {
-    await exec("node", [jestDependencyFile])
+    await exec("node", [jestDependencyFile, "--", ...jestArgs])
   } else {
     const npxPath = await which("npx", true)
     console.warn("Jest not found in %s, using %s instead to install and run it", jestDependencyFile, npxPath)
-    await exec(npxPath, ["jest"])
+    await exec(npxPath, ["jest", ...jestArgs])
   }
 }
 
