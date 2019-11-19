9478b2f5a982f9ee1b7a2f80dedecfccbb40c9f7
Jaid <jaid.jsx@gmail.com>
2019-11-19 11:57:07 +0100

    Testing
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import {exec} from "@actions/exec"
 import {which} from "@actions/io"
 
 async function main() {
+  const statsFile = path.resolve("dist", "jest", "stats.json")
   const jestArgs = [
     "--passWithNoTests",
     "--coverage",
     "--outputFile",
-    path.resolve("dist", "jest", "stats.json"),
+    statsFile,
   ]
   const jestDependencyFile = path.resolve("node_modules", "jest", "bin", "jest.js")
   const isJestInstalled = await fsp.pathExists(jestDependencyFile)
@@ -20,6 +21,8 @@ async function main() {
     console.warn("Jest not found in %s, using %s instead to install and run it", jestDependencyFile, npxPath)
     await exec(npxPath, ["jest", ...jestArgs])
   }
+  const stats = await fsp.readJson(statsFile)
+  console.warn(stats)
 }
 
 main()
