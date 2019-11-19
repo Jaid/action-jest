7b93d3c28a897de00adb6dee31e04fd54ff53938
Jaid <jaid.jsx@gmail.com>
2019-11-19 14:17:44 +0100

    Added option `logHeapUsage`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 import path from "path"
 
+import filterNil from "filter-nil"
 import fsp from "@absolunet/fsp"
+import {getInput} from "@actions/core"
 import {exec} from "@actions/exec"
 import {which} from "@actions/io"
 
 async function main() {
+  const logHeapUsage = getInput("logHeapUsage", {required: true})
   const statsFile = path.resolve("dist", "jest", "stats.json")
   const jestArgs = [
     "--ci",
-    "--color=false",
     "--passWithNoTests",
     "--coverage",
     "--json",
-    `--outputFile="${statsFile}"`,
-  ]
+    "--outputFile",
+    logHeapUsage ? "--logHeapUsage" : null,
+    "--runInBand",
+    statsFile,
+  ] |> filterNil
   const jestDependencyFile = path.resolve("node_modules", "jest", "bin", "jest.js")
   const isJestInstalled = await fsp.pathExists(jestDependencyFile)
   if (isJestInstalled) {
-    await exec("node", [jestDependencyFile, ...jestArgs])
+    const nodeArgs = [
+      logHeapUsage ? "--expose-gc" : null,
+      jestDependencyFile,
+      ...jestArgs,
+    ] |> filterNil
+    await exec("node", nodeArgs)
   } else {
     const npxPath = await which("npx", true)
     console.warn("Jest not found in %s, using %s instead to install and run it", jestDependencyFile, npxPath)
