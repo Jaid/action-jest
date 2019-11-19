98c5cf0dc1c7dfe35e6aff243f5528865a381159
Jaid <jaid.jsx@gmail.com>
2019-11-19 08:27:10 +0100

    Improved structure
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+import path from "path"
+
+import fsp from "@absolunet/fsp"
 import {exec} from "@actions/exec"
 import {which} from "@actions/io"
 
 async function main() {
-  const npxPath = await which("npx", true)
-  console.log(`Npx path: ${npxPath}`)
-  console.log(`cwd: ${require("fs").readdirSync(require("path").resolve("")).join(" ")}`)
-  await exec(npxPath, ["jest"])
+  const jestDependencyFile = path.resolve("node_modules", "jest", "bin", "jest.js")
+  const isJestInstalled = await fsp.pathExists(jestDependencyFile)
+  if (isJestInstalled) {
+    await exec("node", [jestDependencyFile])
+  } else {
+    const npxPath = await which("npx", true)
+    console.warn("Jest not found in %s, using %s instead to install and run it", jestDependencyFile, npxPath)
+    await exec(npxPath, ["jest"])
+  }
 }
 
 main()
