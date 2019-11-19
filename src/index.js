2cd16f4edcfa19c564d9943c0de672c21d6c027a
Jaid <jaid.jsx@gmail.com>
2019-11-19 06:59:38 +0100

    Added debug logging
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import {exec} from "@actions/exec"
 import {which} from "@actions/io"
+import {debug} from "@actions/core"
 
 async function main() {
   const npxPath = await which("npx", true)
+  debug(`Npx path: ${npxPath}`)
   await exec.exec(npxPath, "jest", "--help")
 }
 
