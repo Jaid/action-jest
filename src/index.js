234367a10eee7bd168b4eee216106ed18857f6c5
Jaid <jaid.jsx@gmail.com>
2019-11-19 07:16:47 +0100

    Testing
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {which} from "@actions/io"
 async function main() {
   const npxPath = await which("npx", true)
   console.log(`Npx path: ${npxPath}`)
-  console.log(`cwd: ${require("path").resolve()}`)
+  console.log(`cwd: ${require("fs").readdirSync("")}`)
   await exec(npxPath, ["jest", "--help"])
 }
 
