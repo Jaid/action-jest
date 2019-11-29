dc6c28dbe6db61d5eaf0b0d3bc8c42023c767964
Jaid <jaid.jsx@gmail.com>
2019-11-29 03:09:49 +0100

    Added `lib/getBooleanInput`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/getBooleanInput.js
+++ src/lib/getBooleanInput.js
@@ -1,1 +1,6 @@
-const prepareActionJest = getBooleanInput("prepareActionJest", {required: true})
+import {getInput} from "@actions/core"
+
+export default function (key, options) {
+  const value = getInput(key, options)
+  return /^\s*(true|1)\s*$/i.test(value)
+}
